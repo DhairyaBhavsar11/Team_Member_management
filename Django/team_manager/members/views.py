2e88a13af92f544c69a6d93ac56cc9b1cbc8984b
Dhairya <105397181+DhairyaBhavsar11@users.noreply.github.com>
2023-02-07 15:10:50 -0500

    Change views.py to takes a web request & returns.

--- Django/team_manager/members/views.py
+++ Django/team_manager/members/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import TeamMember
 
-# Create your views here.
+def list_view(request):
+    team_members = TeamMember.objects.all()
+    return render(request, 'members/templates/list.html', {'team_members': team_members})
+
+def add_view(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        role = request.POST['role']
+        TeamMember.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone,
+            email=email,
+            role=role,
+        )
+        return redirect('list')
+    return render(request, 'members/templates/add.html')
+
+def edit_view(request, pk):
+    team_member = TeamMember.objects.get(pk=pk)
+    if request.method == 'POST':
+        team_member.first_name = request.POST['first_name']
+        team_member.last_name = request.POST['last_name']
+        team_member.phone = request.POST['phone']
+        team_member.email = request.POST['email']
+        team_member.role = request.POST['role']
+        team_member.save()
+        return redirect('list')
+    return render(request, 'members/templates/edit.html', {'team_member': team_member})
+
+def delete_view(request, pk):
+    team_member = TeamMember.objects.get(pk=pk)
+    team_member.delete()
+    return redirect('list')
