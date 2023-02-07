ffad536d9dfc1c55fcf1d8a12813e9122e9a0c7a
Dhairya <105397181+DhairyaBhavsar11@users.noreply.github.com>
2023-02-07 14:49:40 -0500

    Update model.py to define the database.

--- Django/team_manager/members/models.py
+++ Django/team_manager/members/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class TeamMember(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    ROLE_CHOICES = [
+        ('regular', 'Regular'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(
+        max_length=7,
+        choices=ROLE_CHOICES,
+        default='regular',
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
